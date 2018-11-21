9c4ce6f557c32d326fa0db10711cd2ca91ec5fdd
Aidan Zerwer <robobug9@gmail.com>
2018-11-21 15:23:38 -0500

    Init stuff

---
+++ Scouting2019/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Scouting2019
+//
+//  Created by Aidan Zerwer on 2018-11-21.
+//  Copyright © 2018 4069. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
