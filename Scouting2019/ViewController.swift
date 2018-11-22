307c83546714c1a32af11e217666e09fec61ca85
Aidan Zerwer <robobug9@gmail.com>
2018-11-22 14:44:10 -0500

    Basic gui setup

--- Scouting2019/ViewController.swift
+++ Scouting2019/ViewController.swift
@@ -9,12 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var elementTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    
+    @IBAction func changeToDesign(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "toDesignSegue", sender: self)
+    }
+    
+    @IBAction func changeToMain(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "toMainSegue", sender: self)
+    }
+    
+    @IBAction func saveDesign(_ sender: UIBarButtonItem) {
+    }
 }
 
